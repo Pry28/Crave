c8df8b4f7866ac59b95480f75d1f42dd3a75c742
olzhas0986 <89378221+Olzhas-Kdyr@users.noreply.github.com>
2025-08-06 01:10:33 +0500

    Update build.sh

--- build.sh
+++ build.sh
@@ -5,8 +5,8 @@ rm -rf .repo/local_manifests
 #repo init -u https://github.com/PixelOS-AOSP/manifest.git -b sixteen --git-lfs
 #repo init -u https://github.com/LineageOS/android.git -b lineage-23.0 --git-lfs
 #repo init -u https://github.com/AxionAOSP/android.git -b lineage-23.0 --git-lfs
-#repo init -u https://github.com/Evolution-X/manifest -b bka --git-lfs
-repo init -u https://github.com/yaap/manifest.git -b sixteen --git-lfs
+repo init -u https://github.com/Evolution-X/manifest -b bka --git-lfs
+#repo init -u https://github.com/yaap/manifest.git -b sixteen --git-lfs
 #repo init --no-repo-verify --git-lfs -u https://github.com/ProjectInfinity-X/manifest -b 16 -g default,-mips,-darwin,-notdefault
 #repo init -u https://github.com/Lunaris-AOSP/android -b 16 --git-lfs
 
@@ -29,7 +29,7 @@ packages/apps/ViPER4AndroidFX
 rm -rf "${mf[@]}"
 
 # Deivce Tree
-git clone https://github.com/PocoF4Trees/device_xiaomi_munch -b yaap device/xiaomi/munch
+git clone https://github.com/PocoF4Trees/device_xiaomi_munch -b 16.0 device/xiaomi/munch
 
 # Vendor Tree
 git clone https://github.com/PocoF4Trees/vendor_xiaomi_munch -b 16 vendor/xiaomi/munch
@@ -38,7 +38,7 @@ git clone https://github.com/PocoF4Trees/vendor_xiaomi_munch -b 16 vendor/xiaomi
 git clone https://github.com/PocoF4Trees/vendor_xiaomi_munch-firmware vendor/xiaomi/munch-firmware
 
 # Kernel Tree
-git clone https://github.com/PocoF4Trees/kernel_xiaomi_munch -b yaap-munch kernel/xiaomi/munch
+git clone https://github.com/PocoF4Trees/kernel_xiaomi_munch -b munch kernel/xiaomi/munch
 
 # Hardware Xiaomi
 git clone https://github.com/PocoF4Trees/hardware_xiaomi -b 16 hardware/xiaomi
@@ -57,5 +57,5 @@ git clone https://github.com/PocoF4Trees/packages_apps_ViPER4AndroidFX packages/
 
 # Build
 . build/envsetup.sh
-lunch yaap_munch-user
-m yaap
+lunch lineage_munch-bp2a-user
+m evolution 
