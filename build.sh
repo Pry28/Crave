e69ce93eede789c78d3b5c790c6e3f7574f2461b
olzhas0986 <89378221+Olzhas-Kdyr@users.noreply.github.com>
2025-08-06 22:46:38 +0500

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,17 +1,17 @@
-rm -rf .repo/local_manifests
+#rm -rf .repo/local_manifests
 
 # repo init
 
 #repo init -u https://github.com/PixelOS-AOSP/manifest.git -b sixteen --git-lfs
 #repo init -u https://github.com/LineageOS/android.git -b lineage-23.0 --git-lfs
 #repo init -u https://github.com/AxionAOSP/android.git -b lineage-23.0 --git-lfs
-repo init -u https://github.com/Evolution-X/manifest -b bka --git-lfs
+#repo init -u https://github.com/Evolution-X/manifest -b bka --git-lfs
 #repo init -u https://github.com/yaap/manifest.git -b sixteen --git-lfs
 #repo init --no-repo-verify --git-lfs -u https://github.com/ProjectInfinity-X/manifest -b 16 -g default,-mips,-darwin,-notdefault
 #repo init -u https://github.com/Lunaris-AOSP/android -b 16 --git-lfs
 
 # repo sync script
-/opt/crave/resync.sh
+#/opt/crave/resync.sh
 
 # Remove old device specific repos
 mf=(
@@ -36,7 +36,7 @@ git clone https://github.com/PocoF4Trees/vendor_xiaomi_munch -b 16 vendor/xiaomi
 git clone https://github.com/PocoF4Trees/vendor_xiaomi_munch-firmware vendor/xiaomi/munch-firmware
 
 # Kernel Tree
-git clone https://github.com/PocoF4Trees/kernel_xiaomi_munch -b munch kernel/xiaomi/munch
+git clone https://github.com/PocoF4Trees/kernel_xiaomi_munch -b yaap-munch kernel/xiaomi/munch
 
 # Hardware Xiaomi
 git clone https://github.com/PocoF4Trees/hardware_xiaomi -b 16 hardware/xiaomi
