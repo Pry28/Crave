c0e662d277a43c16a672f04253d472f00fa69642
olzhas0986 <89378221+Olzhas-Kdyr@users.noreply.github.com>
2025-09-11 10:33:34 +0500

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 rm -rf .repo/local_manifests
 
 # repo init
-repo init --no-repo-verify --git-lfs -u https://github.com/ProjectInfinity-X/manifest -b 16 -g default,-mips,-darwin,-notdefault
+repo init -u https://github.com/Lunaris-AOSP/android -b 16 --git-lfs
 
 # repo sync script
 /opt/crave/resync.sh
@@ -20,40 +20,40 @@ packages/apps/ViPER4AndroidFX
 rm -rf "${remove[@]}"
 
 # Deivce Tree
-git clone https://github.com/PocoF4Trees/device_xiaomi_munch device/xiaomi/munch
+git clone --depth=1 https://github.com/PocoF4Trees/device_xiaomi_munch -b LunarisAOSP device/xiaomi/munch
 
 # Vendor Tree
-git clone https://github.com/PocoF4Trees/vendor_xiaomi_munch vendor/xiaomi/munch
+git clone --depth=1 https://github.com/PocoF4Trees/vendor_xiaomi_munch vendor/xiaomi/munch
 
 # Munch FW
-git clone https://github.com/PocoF4Trees/vendor_xiaomi_munch-firmware vendor/xiaomi/munch-firmware
+git clone --depth=1 https://github.com/PocoF4Trees/vendor_xiaomi_munch-firmware vendor/xiaomi/munch-firmware
 
 # Kernel Tree
-git clone https://github.com/Olzhas-Kdyr/kernel_xiaomi_sm8250 kernel/xiaomi/munch
+git clone --depth=1 https://github.com/Olzhas-Kdyr/kernel_xiaomi_sm8250 kernel/xiaomi/munch
 
 # Hardware Xiaomi
-git clone https://github.com/PocoF4Trees/hardware_xiaomi hardware/xiaomi
+git clone --depth=1 https://github.com/PocoF4Trees/hardware_xiaomi hardware/xiaomi
 
 # Hardware Dolby Atmos
-git clone https://github.com/PocoF4Trees/hardware_dolby hardware/dolby
+git clone --depth=1 https://github.com/PocoF4Trees/hardware_dolby hardware/dolby
 
 # DeviceSettings
-git clone https://github.com/PocoF4Trees/packages_resources_devicesettings packages/resources/devicesettings
+git clone --depth=1 https://github.com/PocoF4Trees/packages_resources_devicesettings packages/resources/devicesettings
 
 # MIUI Camera
-git clone https://github.com/PocoF4Trees/vendor_xiaomi_miuicamera vendor/xiaomi/miuicamera
+git clone --depth=1 https://github.com/PocoF4Trees/vendor_xiaomi_miuicamera vendor/xiaomi/miuicamera
 
 # ViPER4AndroidFX
-git clone https://github.com/PocoF4Trees/packages_apps_ViPER4AndroidFX packages/apps/ViPER4AndroidFX
+git clone --depth=1 https://github.com/PocoF4Trees/packages_apps_ViPER4AndroidFX packages/apps/ViPER4AndroidFX
 
 # Build
 . build/envsetup.sh
-lunch infinity_munch-user
-m bacon
+lunch lineage_munch-bp2a-user
+m lunaris
 
 cd out/target/product && rm -rf gapps && mv munch gapps && cd ../../..
-cd device/xiaomi/munch && rm -rf infinity_munch.mk && mv vanilla.txt infinity_munch.mk && cd ../../..
+cd device/xiaomi/munch && rm -rf lineage_munch.mk && mv vanilla.txt lineage_munch.mk && cd ../../..
 
 . build/envsetup.sh
-lunch infinity_munch-user
-m bacon
+lunch lineage_munch-bp2a-user
+m lunaris
