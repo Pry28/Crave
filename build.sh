10d2c15403656759a7a42ee17703ffa49bad227a
olzhas0986 <89378221+Olzhas-Kdyr@users.noreply.github.com>
2025-10-04 16:47:46 +0500

    Update build.sh

--- build.sh
+++ build.sh
@@ -20,47 +20,54 @@ packages/apps/ViPER4AndroidFX
 rm -rf "${remove[@]}"
 
 # Deivce Tree
-git clone --depth=1 https://github.com/Olzhas-Kdyr/android_device_xiaomi_munch -b LunarisAOSP-16 device/xiaomi/munch
-git clone --depth=1 https://github.com/Olzhas-Kdyr/android_device_xiaomi_sm8250-common device/xiaomi/sm8250-common
+git clone --depth=1 https://github.com/PocoF4Trees/device_xiaomi_munch.git device/xiaomi/munch
+#git clone --depth=1 https://github.com/Olzhas-Kdyr/android_device_xiaomi_sm8250-common device/xiaomi/sm8250-common
 
 # Vendor Tree
-git clone --depth=1 https://github.com/Olzhas-Kdyr/proprietary_vendor_xiaomi_munch vendor/xiaomi/munch
-git clone --depth=1 https://github.com/Olzhas-Kdyr/proprietary_vendor_xiaomi_sm8250-common vendor/xiaomi/sm8250-common
+git clone --depth=1 https://github.com/PocoF4Trees/vendor_xiaomi_munch.git vendor/xiaomi/munch
+#git clone --depth=1 https://github.com/Olzhas-Kdyr/proprietary_vendor_xiaomi_sm8250-common vendor/xiaomi/sm8250-common
 
 # Munch FW
 git clone --depth=1 https://github.com/PocoF4Trees/vendor_xiaomi_munch-firmware vendor/xiaomi/munch-firmware
 
 # Kernel Tree
-git clone --depth=1 https://github.com/SenseiiX/fusionX_sm8250.git -b wip-next kernel/xiaomi/sm8250
+git clone --depth=1 https://github.com/SenseiiX/fusionX_sm8250.git -b wip-next kernel/xiaomi/munch
 
 # Hardware Xiaomi
-git clone --depth=1 https://github.com/Olzhas-Kdyr/android_hardware_xiaomi.git -b lineage-23.0 hardware/xiaomi
+git clone https://github.com/Project-SenX/android_hardware_xiaomi hardware/xiaomi
 
 # DeviceSettings
-git clone --depth=1 https://github.com/PocoF4Trees/packages_resources_devicesettings packages/resources/devicesettings
+git clone https://github.com/PocoF4Trees/packages_resources_devicesettings packages/resources/devicesettings
 
 # MIUI Camera
 git clone --depth=1 https://github.com/PocoF4Trees/vendor_xiaomi_miuicamera vendor/xiaomi/miuicamera
 
 # ViPER4AndroidFX
-git clone https://github.com/PocoF4Trees/packages_apps_ViPER4AndroidFX packages/apps/ViPER4AndroidFX
-
-cd kernel/xiaomi/sm8250
-bash nextpatch.sh
-rm -rf KernelSU-Next/userspace/su
-cd ../../..
+#git clone https://github.com/PocoF4Trees/packages_apps_ViPER4AndroidFX packages/apps/ViPER4AndroidFX
 
 . build/envsetup.sh
 export BUILD_USERNAME=olzhas
 export BUILD_HOSTNAME=ubuntu
-lunch lineage_munch-bp2a-userdebug
+
+cd kernel/xiaomi/munch
+bash nextpatch.sh
+rm -rf KernelSU-Next/userspace/su
+cd ../../..
+
+lunch lineage_munch-bp2a-user
 m lunaris
 
-#cd out/target/product && rm -rf gapps && mv munch gapps && cd ../../..
-#cd device/xiaomi/munch && rm -rf lineage_munch.mk && mv vanilla.txt lineage_munch.mk && cd ../../..
+cd out/target/product && rm -rf gapps && mv munch gapps && cd ../../..
+cd device/xiaomi/munch && rm -rf lineage_munch.mk && mv vanilla.txt lineage_munch.mk && cd ../../..
 
-#. build/envsetup.sh
-#export BUILD_USERNAME=olzhas
-#export BUILD_HOSTNAME=ubuntu
-#lunch lineage_munch-bp1a-user
-#mka bacon
+. build/envsetup.sh
+
+cd kernel/xiaomi/munch
+bash nextpatch.sh
+rm -rf KernelSU-Next/userspace/su
+cd ../../..
+
+export BUILD_USERNAME=olzhas
+export BUILD_HOSTNAME=ubuntu
+lunch lineage_munch-bp2a-user
+m lunaris
