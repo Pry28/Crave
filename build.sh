52616f2b2ffc9c96519894b8e480156bec2c3c41
olzhas0986 <89378221+Olzhas-Kdyr@users.noreply.github.com>
2025-10-05 11:01:00 +0500

    Update build.sh

--- build.sh
+++ build.sh
@@ -20,7 +20,7 @@ packages/apps/ViPER4AndroidFX
 rm -rf "${remove[@]}"
 
 # Deivce Tree
-git clone --depth=1 https://github.com/Olzhas-Kdyr/android_device_xiaomi_munch device/xiaomi/munch
+git clone --depth=1 https://github.com/Olzhas-Kdyr/android_device_xiaomi_munch -b LunarisAOSP-16 device/xiaomi/munch
 git clone --depth=1 https://github.com/Olzhas-Kdyr/android_device_xiaomi_sm8250-common -b los-parts device/xiaomi/sm8250-common
 
 # Vendor Tree
