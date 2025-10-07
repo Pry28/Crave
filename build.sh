741495081ad34d83dc40416020a5b8d74ac087c2
olzhas0986 <89378221+Olzhas-Kdyr@users.noreply.github.com>
2025-10-07 09:46:38 +0500

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,7 +1,8 @@
 rm -rf .repo/local_manifests
 
 # repo init
-repo init -u https://github.com/Lunaris-AOSP/android -b 16 --git-lfs
+#repo init -u https://github.com/Lunaris-AOSP/android -b 16 --git-lfs
+repo init --no-repo-verify --git-lfs -u https://github.com/ProjectInfinity-X/manifest -b 16 -g default,-mips,-darwin,-notdefault
 
 # repo sync script
 /opt/crave/resync.sh
@@ -20,8 +21,8 @@ packages/apps/ViPER4AndroidFX
 rm -rf "${remove[@]}"
 
 # Deivce Tree
-git clone --depth=1 https://github.com/Olzhas-Kdyr/android_device_xiaomi_munch -b LunarisAOSP-16 device/xiaomi/munch
-git clone --depth=1 https://github.com/Olzhas-Kdyr/android_device_xiaomi_sm8250-common -b test device/xiaomi/sm8250-common
+git clone --depth=1 https://github.com/Olzhas-Kdyr/android_device_xiaomi_munch -b InfinityX-16 device/xiaomi/munch
+git clone --depth=1 https://github.com/Olzhas-Kdyr/android_device_xiaomi_sm8250-common -b InfinityX-16 device/xiaomi/sm8250-common
 
 # Vendor Tree
 git clone --depth=1 https://github.com/Olzhas-Kdyr/proprietary_vendor_xiaomi_munch vendor/xiaomi/munch
@@ -31,7 +32,7 @@ git clone --depth=1 https://github.com/Olzhas-Kdyr/proprietary_vendor_xiaomi_sm8
 git clone --depth=1 https://github.com/PocoF4Trees/vendor_xiaomi_munch-firmware vendor/xiaomi/munch-firmware
 
 # Kernel Tree
-git clone --depth=1 https://github.com/SenseiiX/fusionX_sm8250.git -b wip-next kernel/xiaomi/sm8250
+git clone --depth=1 https://github.com/munch-devs/kernel_xiaomi_munch -b new-ksu kernel/xiaomi/sm8250
 
 # Hardware Xiaomi
 git clone https://github.com/Project-SenX/android_hardware_xiaomi hardware/xiaomi
@@ -53,24 +54,24 @@ export BUILD_USERNAME=olzhas
 export BUILD_HOSTNAME=ubuntu
 
 cd kernel/xiaomi/sm8250
-bash nextpatch.sh
+git submodule update --init
 rm -rf KernelSU-Next/userspace/su
 cd ../../..
 
-lunch lineage_munch-bp2a-user
-m lunaris
+lunch infinity_munch-user
+m bacon
 
-#cd out/target/product && rm -rf gapps && mv munch gapps && cd ../../..
-#cd device/xiaomi/munch && rm -rf lineage_munch.mk && mv vanilla.txt lineage_munch.mk && cd ../../..
+cd out/target/product && rm -rf gapps && mv munch gapps && cd ../../..
+cd device/xiaomi/munch && rm -rf infinity_munch.mk && mv vanilla.txt infinity_munch.mk && cd ../../..
 
-#. build/envsetup.sh
+. build/envsetup.sh
 
-#cd kernel/xiaomi/munch
-#bash nextpatch.sh
-#rm -rf KernelSU-Next/userspace/su
-#cd ../../..
+cd kernel/xiaomi/sm8250
+git submodule update --init
+rm -rf KernelSU-Next/userspace/su
+cd ../../..
 
-#export BUILD_USERNAME=olzhas
-#export BUILD_HOSTNAME=ubuntu
-#lunch lineage_munch-bp2a-user
-#m lunaris
+export BUILD_USERNAME=olzhas
+export BUILD_HOSTNAME=ubuntu
+lunch infinity_munch-user
+m bacon
