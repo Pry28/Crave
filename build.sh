ce178634e014e0fece25e4103063522f617651b4
olzhas0986 <89378221+Olzhas-Kdyr@users.noreply.github.com>
2025-10-11 19:14:59 +0500

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,8 +1,8 @@
 rm -rf .repo/local_manifests
 
 # repo init
-#repo init -u https://github.com/Lunaris-AOSP/android -b 16 --git-lfs
-repo init --no-repo-verify --git-lfs -u https://github.com/ProjectInfinity-X/manifest -b 16 -g default,-mips,-darwin,-notdefault
+repo init -u https://github.com/Lunaris-AOSP/android -b 16 --git-lfs
+#repo init --no-repo-verify --git-lfs -u https://github.com/ProjectInfinity-X/manifest -b 16 -g default,-mips,-darwin,-notdefault
 
 # repo sync script
 /opt/crave/resync.sh
@@ -21,8 +21,8 @@ packages/apps/ViPER4AndroidFX
 rm -rf "${remove[@]}"
 
 # Deivce Tree
-git clone --depth=1 https://github.com/Olzhas-Kdyr/android_device_xiaomi_munch -b InfinityX-16 device/xiaomi/munch
-git clone --depth=1 https://github.com/Olzhas-Kdyr/android_device_xiaomi_sm8250-common -b InfinityX-16 device/xiaomi/sm8250-common
+git clone --depth=1 https://github.com/Olzhas-Kdyr/android_device_xiaomi_munch -b LunarisAOSP-16 device/xiaomi/munch
+git clone --depth=1 https://github.com/Olzhas-Kdyr/android_device_xiaomi_sm8250-common -b Lunatest device/xiaomi/sm8250-common
 
 # Vendor Tree
 git clone --depth=1 https://github.com/Olzhas-Kdyr/proprietary_vendor_xiaomi_munch vendor/xiaomi/munch
@@ -58,22 +58,22 @@ chmod +x ksupatch.sh
 bash ksupatch.sh
 cd ../../..
 
-lunch infinity_munch-user
-m bacom
+lunch lineage_munch-bp2a-user
+m lunaris 
 
-cd out/target/product && rm -rf gapps && mv munch gapps && cd ../../..
-cd device/xiaomi/munch && rm -rf infinity_munch.mk && mv vanilla.txt infinity_munch.mk && cd ../../..
+#cd out/target/product && rm -rf gapps && mv munch gapps && cd ../../..
+#cd device/xiaomi/munch && rm -rf infinity_munch.mk && mv vanilla.txt infinity_munch.mk && cd ../../..
 
-. build/envsetup.sh
+#. build/envsetup.sh
 
-cd kernel/xiaomi/sm8250
+#cd kernel/xiaomi/sm8250
 #git submodule update --init
 #rm -rf KernelSU-Next/userspace/su
-chmod +x ksupatch.sh
-bash ksupatch.sh
-cd ../../..
+#chmod +x ksupatch.sh
+#bash ksupatch.sh
+#cd ../../..
 
-export BUILD_USERNAME=olzhas
-export BUILD_HOSTNAME=ubuntu
-lunch infinity_munch-user
-m bacon
+#export BUILD_USERNAME=olzhas
+#export BUILD_HOSTNAME=ubuntu
+#lunch infinity_munch-user
+#m bacon
