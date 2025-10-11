7b053e830695c92e67f6faa91f000c1ad92d7aeb
olzhas0986 <89378221+Olzhas-Kdyr@users.noreply.github.com>
2025-10-11 19:36:15 +0500

    Update build.sh

--- build.sh
+++ build.sh
@@ -61,19 +61,19 @@ cd ../../..
 lunch lineage_munch-bp2a-user
 m lunaris 
 
-#cd out/target/product && rm -rf gapps && mv munch gapps && cd ../../..
-#cd device/xiaomi/munch && rm -rf infinity_munch.mk && mv vanilla.txt infinity_munch.mk && cd ../../..
+cd out/target/product && rm -rf gapps && mv munch gapps && cd ../../..
+cd device/xiaomi/munch && rm -rf lineage_munch.mk && mv vanilla.txt lineage_munch.mk && cd ../../..
 
-#. build/envsetup.sh
+. build/envsetup.sh
 
-#cd kernel/xiaomi/sm8250
+cd kernel/xiaomi/sm8250
 #git submodule update --init
 #rm -rf KernelSU-Next/userspace/su
-#chmod +x ksupatch.sh
-#bash ksupatch.sh
-#cd ../../..
+chmod +x ksupatch.sh
+bash ksupatch.sh
+cd ../../..
 
-#export BUILD_USERNAME=olzhas
-#export BUILD_HOSTNAME=ubuntu
-#lunch infinity_munch-user
-#m bacon
+export BUILD_USERNAME=olzhas
+export BUILD_HOSTNAME=ubuntu
+lunch lineage_munch-bp2a-user
+m lunaris 
