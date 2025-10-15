e5a8b8e9db4d8c7f0677f05007a414076474123c
olzhas0986 <89378221+Olzhas-Kdyr@users.noreply.github.com>
2025-10-16 00:53:01 +0500

    Update build.sh

--- build.sh
+++ build.sh
@@ -32,7 +32,7 @@ git clone --depth=1 https://github.com/Olzhas-Kdyr/proprietary_vendor_xiaomi_sm8
 git clone --depth=1 https://github.com/PocoF4Trees/vendor_xiaomi_munch-firmware vendor/xiaomi/munch-firmware
 
 # Kernel Tree
-git clone --depth=1 https://github.com/PocoF4Trees/kernel_xiaomi_sm8250.git -b staging-test kernel/xiaomi/sm8250
+git clone --depth=1 https://github.com/PocoF4Trees/kernel_xiaomi_sm8250.git kernel/xiaomi/sm8250
 
 # Hardware Xiaomi
 git clone https://github.com/Project-SenX/android_hardware_xiaomi hardware/xiaomi
@@ -43,16 +43,13 @@ git clone https://github.com/PocoF4Trees/packages_resources_devicesettings packa
 # MIUI Camera
 git clone --depth=1 https://github.com/PocoF4Trees/vendor_xiaomi_miuicamera vendor/xiaomi/miuicamera
 
-# Hardware Dolby Atmos
-#git clone --depth=1 https://github.com/PocoF4Trees/hardware_dolby hardware/dolby
-
 # ViPER4AndroidFX
 git clone https://github.com/PocoF4Trees/packages_apps_ViPER4AndroidFX packages/apps/ViPER4AndroidFX
 
 # Signing
-git clone https://github.com/Evolution-X/vendor_evolution-priv_keys-template vendor/lunaris-priv/keys
-cd vendor/lunaris-priv/keys
-./keys.sh
+#git clone https://github.com/Evolution-X/vendor_evolution-priv_keys-template vendor/lunaris-priv/keys
+#cd vendor/lunaris-priv/keys
+#./keys.sh
 
 # Building 
 . build/envsetup.sh
@@ -60,7 +57,6 @@ export BUILD_USERNAME=olzhas
 export BUILD_HOSTNAME=ubuntu
 # Building: Cloning KSU Next
 cd kernel/xiaomi/sm8250
-chmod +x ksupatch.sh
 bash ksupatch.sh
 cd ../../..
 
