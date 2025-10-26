f3217207fa4345566214b2e343e609a3c4eec17a
olzhas0986 <89378221+Olzhas-Kdyr@users.noreply.github.com>
2025-10-26 07:20:45 +0500

    Update build.sh

--- build.sh
+++ build.sh
@@ -10,6 +10,7 @@ device/xiaomi
 kernel/xiaomi
 vendor/xiaomi
 hardware/xiaomi
+packages/resources/devicesettings
 vendor/lineage-priv/keys
 )
 
@@ -34,6 +35,9 @@ git clone https://github.com/Olzhas-Kdyr/android_hardware_xiaomi.git hardware/xi
 
 # MIUI Camera
 git clone https://github.com/PocoF4Trees/vendor_xiaomi_miuicamera vendor/xiaomi/miuicamera
+
+# devicesettings
+git clone https://github.com/PocoF3Releases/packages_resources_devicesettings.git packages/resources/devicesettings
 
 # My Keys
 git clone https://github.com/Olzhas-Kdyr/keys.git vendor/lineage-priv/keys
