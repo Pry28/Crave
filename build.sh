1d18d2700bd68e890d3dd3dad22aa14d395d9b65
olzhas0986 <89378221+Olzhas-Kdyr@users.noreply.github.com>
2025-10-29 09:41:05 +0500

    Update build.sh

--- build.sh
+++ build.sh
@@ -12,14 +12,13 @@ vendor/xiaomi
 hardware/xiaomi
 packages/resources/devicesettings
 vendor/lineage-priv/keys
-out/target/product/munch
 )
 
 rm -rf "${remove[@]}"
 
 # Deivce Trees
 git clone https://github.com/Olzhas-Kdyr/android_device_xiaomi_munch device/xiaomi/munch
-git clone https://github.com/Olzhas-Kdyr/android_device_xiaomi_sm8250-common device/xiaomi/sm8250-common
+git clone https://github.com/Olzhas-Kdyr/android_device_xiaomi_sm8250-common -b crtest device/xiaomi/sm8250-common
 
 # Vendor Trees
 git clone https://github.com/Olzhas-Kdyr/proprietary_vendor_xiaomi_munch vendor/xiaomi/munch
