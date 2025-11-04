1aa1f30d70a8ceb72a0b7b7ef16f39c60e75bb11
olzhas0986 <89378221+Olzhas-Kdyr@users.noreply.github.com>
2025-11-05 00:02:31 +0500

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,5 +1,6 @@
 # repo init
-repo init -u https://github.com/crdroidandroid/android.git -b 16.0 --git-lfs --no-clone-bundle
+#repo init -u https://github.com/crdroidandroid/android.git -b 16.0 --git-lfs --no-clone-bundle
+repo init --depth=1 --no-repo-verify --git-lfs -u https://github.com/ProjectInfinity-X/manifest -b 16 -g default,-mips,-darwin,-notdefault
 
 # repo sync script
 /opt/crave/resync.sh
@@ -17,8 +18,8 @@ vendor/lineage-priv/keys
 rm -rf "${remove[@]}"
 
 # Deivce Trees
-git clone https://github.com/Olzhas-Kdyr/android_device_xiaomi_munch device/xiaomi/munch
-git clone https://github.com/Olzhas-Kdyr/android_device_xiaomi_sm8250-common -b cleanup device/xiaomi/sm8250-common
+git clone https://github.com/Olzhas-Kdyr/android_device_xiaomi_munch -b infinity-16 device/xiaomi/munch
+git clone https://github.com/Olzhas-Kdyr/android_device_xiaomi_sm8250-common -b infinity-16 device/xiaomi/sm8250-common
 
 # Vendor Trees
 git clone https://github.com/Olzhas-Kdyr/proprietary_vendor_xiaomi_munch vendor/xiaomi/munch
@@ -49,6 +50,6 @@ git clone https://github.com/Olzhas-Kdyr/keys.git vendor/lineage-priv/keys
 export BUILD_USERNAME=olzhas
 export BUILD_HOSTNAME=ubuntu
 export SKIP_ABI_CHECKS=true
-lunch lineage_munch-bp2a-user
+lunch infinity_munch-user
 make installclean
 m bacon
