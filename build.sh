50421e47b87285b94289489e3e4dfaf0fff95a78
olzhas0986 <89378221+Olzhas-Kdyr@users.noreply.github.com>
2025-11-25 20:11:49 +0500

    Update build.sh

--- build.sh
+++ build.sh
@@ -20,7 +20,7 @@ rm -rf "${remove[@]}"
 
 # Deivce Trees
 git clone https://github.com/Olzhas-Kdyr/android_device_xiaomi_munch device/xiaomi/munch
-git clone https://github.com/Olzhas-Kdyr/android_device_xiaomi_sm8250-common.git device/xiaomi/sm8250-common
+git clone https://github.com/Olzhas-Kdyr/android_device_xiaomi_sm8250-common.git -b aosp-16-cleanup device/xiaomi/sm8250-common
 
 # Vendor Trees
 git clone https://github.com/Olzhas-Kdyr/proprietary_vendor_xiaomi_munch vendor/xiaomi/munch
