90c7676e61c50d709d4e5ee91532faec910f9621
olzhas0986 <89378221+Olzhas-Kdyr@users.noreply.github.com>
2025-11-26 07:22:46 +0500

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 # repo init
-repo init -u https://github.com/crdroidandroid/android.git -b 16.0 --git-lfs --no-clone-bundle
-#repo init -u https://github.com/Project-Mica/manifest -b 16-qpr1
+#repo init -u https://github.com/crdroidandroid/android.git -b 16.0 --git-lfs --no-clone-bundle
 #repo init -u https://github.com/ProjectMatrixx/android.git -b 16.0 --git-lfs
+repo init -u https://github.com/Evolution-X/manifest -b bq1 --git-lfs
 
 # repo sync script
 /opt/crave/resync.sh
@@ -19,7 +19,7 @@ vendor/lineage-priv/keys
 rm -rf "${remove[@]}"
 
 # Deivce Trees
-git clone https://github.com/Olzhas-Kdyr/android_device_xiaomi_munch device/xiaomi/munch
+git clone https://github.com/Olzhas-Kdyr/android_device_xiaomi_munch -b evo device/xiaomi/munch
 git clone https://github.com/Olzhas-Kdyr/android_device_xiaomi_sm8250-common.git -b aosp-16-cleanup device/xiaomi/sm8250-common
 
 # Vendor Trees
@@ -30,7 +30,8 @@ git clone https://github.com/Olzhas-Kdyr/proprietary_vendor_xiaomi_sm8250-common
 git clone https://codeberg.org/munch-devs/android_vendor_xiaomi_munch-firmware.git vendor/xiaomi/munch-firmware
 
 # Kernel Tree
-git clone https://github.com/crdroidandroid/android_kernel_xiaomi_sm8250.git -b 16.0-munch kernel/xiaomi/sm8250
+#git clone https://github.com/crdroidandroid/android_kernel_xiaomi_sm8250.git -b 16.0-munch kernel/xiaomi/sm8250
+https://github.com/Khalvat-M/android_kernel_xiaomi_sm8250.git kernel/xiaomi/sm8250
 
 # Hardware Xiaomi
 git clone https://github.com/Olzhas-Kdyr/android_hardware_xiaomi.git -b aosp-16 hardware/xiaomi
@@ -44,16 +45,16 @@ git clone https://github.com/PocoF3Releases/packages_resources_devicesettings.gi
 # My Keys
 git clone https://github.com/Olzhas-Kdyr/keys vendor/lineage-priv/keys
 
-cd kernel/xiaomi/sm8250
-chmod +x ksu.sh
-bash ksu.sh
-rm -rf KernelSU-Next/userspace/su
-cd ../../..
+#cd kernel/xiaomi/sm8250
+#chmod +x ksu.sh
+#bash ksu.sh
+#rm -rf KernelSU-Next/userspace/su
+#cd ../../..
 
 # Building 
 . build/envsetup.sh
 export BUILD_USERNAME=olzhas
 export BUILD_HOSTNAME=ubuntu
 export SKIP_ABI_CHECKS=true
-lunch lineage_munch-bp2a-user
-m bacon
+lunch lineage_munch-bp3a-user
+m evolution 
